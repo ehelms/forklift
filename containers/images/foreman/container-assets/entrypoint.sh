e14409478be4ae3f3cc3b2acf7fe9f46d3867cb5
Eric D. Helms <ericdhelms@gmail.com>
2018-06-29 12:52:36 -0400

    Re-factor Foreman image and deployment

--- containers/images/foreman/container-assets/entrypoint.sh
+++ containers/images/foreman/container-assets/entrypoint.sh
@@ -8,6 +8,6 @@ foreman-rake db:migrate
 echo $SEED_ADMIN_PASSWORD
 SEED_ADMIN_PASSWORD=$SEED_ADMIN_PASSWORD foreman-rake db:seed
 
-/usr/bin/create_qpid_queue.sh
+#/usr/bin/create_qpid_queue.sh
 
 exec "$@"
